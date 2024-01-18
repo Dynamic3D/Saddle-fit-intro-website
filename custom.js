f4eed9e1ff7ef97f1dea1acf469b526958d552a3
Hasnain Khan <itshasnainkhanofficial@gmail.com>
2024-01-18 17:23:43 +0500

    added loader and toast

--- custom.js
+++ custom.js
@@ -1,25 +1,50 @@
+function showLoader() {
+  document.getElementById("loader").style.display = "block";
+}
+
+function hideLoader() {
+  document.getElementById("loader").style.display = "none";
+}
+
 async function subscribe(event) {
-    event.preventDefault();
+  event.preventDefault();
+  var subscribeText = document.getElementById("subscribeText");
+  var email = document.getElementById("email");
 
-    var email = document.getElementById("email").value;
+  if (!email.value) {
+    toastr.error("Please enter your email address");
+    return;
+  }
 
-    try {
-        const response = await fetch("https://wriil2sbhk.execute-api.us-east-1.amazonaws.com/dev/subscribe", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ email: email }),
-        });
+  showLoader();
+  subscribeText.style.display = "none";
 
-        if (response.status === 409) {
-            alert("Email already subscribed");
-        } else if (response.status === 200) {
-            alert("Subscribed successfully");
-        } else {
-            alert("This email is not verified");
-        }
-    } catch (error) {
-        alert("Error in fetching data");
+  try {
+    const response = await fetch(
+      "https://wriil2sbhk.execute-api.us-east-1.amazonaws.com/dev/subscribe",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: email.value }),
+      }
+    );
+
+    hideLoader();
+    email.value = "";
+
+    if (response.status === 409) {
+      toastr.error("Email already subscribed");
+    } else if (response.status === 200) {
+      toastr.success("Subscribed successfully");
+    } else {
+      toastr.error("This email is not verified");
     }
+  } catch (error) {
+    hideLoader();
+    toastr.error("Error in fetching data");
+  } finally {
+    subscribeText.style.display = "inline-block";
+  }
 }
